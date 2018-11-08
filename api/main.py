373cb7dc13274b1c879ed48b6b06ce9056535661
Haitao Cao <2310852380@qq.com>
2018-11-08 19:51:32 +0800

    更新主页
    1.更改了第二篇博客为翻译博客（SimpleCoin简介及部署）
    2.增加自己参与的git项目（Icecream Turling、nab-tall、Python2）

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 {"newsList":[
   {"content":"在网络日益发达的现在，也伴随着有益信息与造成不稳定因素的信息也随之日益泛滥，为了网民的思想健康，也为了社会的和谐，在许多对外公共场合下，有些内容是要经过审查才能显示的。","hint":"查看详情…","id":0,"title":"C++实现敏感词过滤算法","url":"http://blog.csdn.net/little_stupid_child/article/details/54407271"},
-  {"content":"随着比特币的不断发展,它的底层技术区块链也逐步走进公众视野,引起大众注意。本文用不到50行的Python代码构建最小的数据区块链，简单介绍了区块链去中心化的结构与其实现原理。","hint":"去瞅瞅…","id":1,"title":"50行Python代码构建最小的区块链","url":"http://blog.csdn.net/little_stupid_child/article/details/79340443"}],
+  {"content":"SimpleCion是一个简单的，不太安全，不太完整的用Python制作的区块链实现。这个项目的目标是制作一个有效的区块链货币，尽可能的使它简洁，可以用作教材。","hint":"去瞅瞅…","id":1,"title":"区块链SimpleCoin简介及部署","url":"http://blog.csdn.net/little_stupid_child/article/details/79373272"}],
  "person":{
   "avatars":"./images/main/cht_head.png","name":"曹海涛","slogan":"Stay hungry,stay foolish !",
   "tabTags":[{"hint":"我的博客","link":"http://blog.csdn.net/little_stupid_child"},{"hint":"我的Github","link":"https://github.com/StupidTAO"},{"hint":"我的加密猫","link":"https://cloudpet.io/cat/3258"},{"hint":"我的简历","link":"http://blog.csdn.net/little_stupid_child/article/details/79351072"}]},
@@ -8,5 +8,8 @@
                  {"describe":"ONNX 的全称为“Open Neural Network Exchange”，即“开放的神经网络切换”。该项目的目的是让不同的神经网络开发框架做到互通互用。","id":0,"link":"https://github.com/StupidTAO/onnx","name":"ONNX"},
                  {"describe":"洛基是一个C++库的设计，包含常用的设计模式和idioms的灵活的实现。","id":0,"link":"https://github.com/StupidTAO/loki","name":"Loki"},
                  {"describe":"STL是Standard Template Library的简称，中文名标准模板库。STL即为通用算法+通用容器。","id":0,"link":"https://github.com/StupidTAO/STL","name":"STL"},
-                 {"describe":"SimpleCion是一个简单的，不太安全，不太完整的用Python制作的区块链实现。这个项目的目标是制作一个有效的区块链货币，尽可能的使它简洁，可以用作教材。","id":0,"link":"https://github.com/StupidTAO/SimpleCoin","name":"SimpleCoin"}],
+                 {"describe":"SimpleCion是一个简单的，不太安全，不太完整的用Python制作的区块链实现。这个项目的目标是制作一个有效的区块链货币，尽可能的使它简洁，可以用作教材。","id":0,"link":"https://github.com/StupidTAO/SimpleCoin","name":"SimpleCoin"},
+                 {"describe":"这是一个基于第三方与服务开发的Android APP，本项目与Siri功能类似可与用户对话，查询天气、检索信息、智能聊天等功能。","id":0,"link":"https://github.com/StupidTAO/IceCream_Turling","name":"IceCream_Turling"},
+                 {"describe":"Python2包含了很多使用的小程序，Python2解决一些常见的问题，也为初学者提供学习。","id":0,"link":"https://github.com/StupidTAO/Python-2","name":"Python2"},
+                 {"describe":"这个项目是用于聚合知名新闻平台上的NBA新闻，如hupu，腾讯，新浪等，为NBA球迷们提供全面、准确、权威的NBA新闻。","id":0,"link":"https://github.com/StupidTAO/nba-tall","name":"Python2"}],
  "wechat":{"hint":"扫一扫关注曹海涛","image":"./images/main/cht_wechat.png"}}
