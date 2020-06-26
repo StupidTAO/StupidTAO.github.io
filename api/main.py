825d7c09d6d189d03f79ccaa9ff5f2154856586b
StupidTAO <2310852380@qq.com>
2020-06-26 18:14:39 +0800

    fix STL to Apigateway

--- api/main.py
+++ api/main.py
@@ -6,7 +6,7 @@
   "tabTags":[{"hint":"我的博客","link":"http://blog.csdn.net/little_stupid_child"},{"hint":"我的Github","link":"https://github.com/StupidTAO"},{"hint":"我的加密猫","link":"https://cloudpet.io/cat/3258"},{"hint":"我的简历","link":"http://blog.csdn.net/little_stupid_child/article/details/79351072"}]},
   "projectList":[{"describe":"这个仓库主要用于练习acm，题目来源于北大在线评测系统，欢迎大家提交更好的代码！ http://poj.org/problemlist","id":0,"link":"https://github.com/StupidTAO/ACM---PKU-Online-Judge","name":"ACM---PKU-Online-Judge"},
                  {"describe":"Blockchain Papers Reading Roadmap 给出了区块链中各主要模块的论文，并对论文进行评分，以给初学者一些指引。","id":0,"link":"https://github.com/StupidTAO/Blackchain-Papers-Reading-Roadmap","name":"Blackchain-Papers-Reading-Roadmap"},
-                 {"describe":"STL是Standard Template Library的简称，中文名标准模板库。STL即为通用算法+通用容器。","id":0,"link":"https://github.com/StupidTAO/STL","name":"STL"},
+                 {"describe":"apigateway是公有链justitia对外提供的api接口，节点运行后链接至节点，即可获取节点服务","id":0,"link":"https://github.com/StupidTAO/apigateway","name":"Apigateway"},
                  {"describe":"SimpleCion是一个简单的，不太安全，不太完整的用Python制作的区块链实现。这个项目的目标是制作一个有效的区块链货币，尽可能的使它简洁，可以用作教材。","id":0,"link":"https://github.com/StupidTAO/SimpleCoin","name":"SimpleCoin"},
                  {"describe":"这是一个基于第三方与服务开发的Android APP，本项目与Siri功能类似可与用户对话，查询天气、检索信息、智能聊天等功能。","id":0,"link":"https://github.com/StupidTAO/IceCream_Turling","name":"IceCream_Turling"},
                  {"describe":"这个项目是用于聚合知名新闻平台上的NBA新闻，如hupu，腾讯，新浪等，为NBA球迷们提供全面、准确、权威的NBA新闻。","id":0,"link":"https://github.com/StupidTAO/nba-tall","name":"nba-tall"}],
