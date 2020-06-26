be628a6396b5e1ce3d7be71c62fbb92c4a3c5deb
StupidTAO <2310852380@qq.com>
2020-06-26 17:56:22 +0800

    delete loki & python2

--- api/main.py
+++ api/main.py
@@ -6,10 +6,8 @@
   "tabTags":[{"hint":"我的博客","link":"http://blog.csdn.net/little_stupid_child"},{"hint":"我的Github","link":"https://github.com/StupidTAO"},{"hint":"我的加密猫","link":"https://cloudpet.io/cat/3258"},{"hint":"我的简历","link":"http://blog.csdn.net/little_stupid_child/article/details/79351072"}]},
   "projectList":[{"describe":"这个仓库主要用于练习acm，题目来源于北大在线评测系统，欢迎大家提交更好的代码！ http://poj.org/problemlist","id":0,"link":"https://github.com/StupidTAO/ACM---PKU-Online-Judge","name":"ACM---PKU-Online-Judge"},
                  {"describe":"Blockchain Papers Reading Roadmap 给出了区块链中各主要模块的论文，并对论文进行评分，以给初学者一些指引。","id":0,"link":"https://github.com/StupidTAO/Blackchain-Papers-Reading-Roadmap","name":"Blackchain-Papers-Reading-Roadmap"},
-                 {"describe":"洛基是一个C++库的设计，包含常用的设计模式和idioms的灵活的实现。","id":0,"link":"https://github.com/StupidTAO/loki","name":"Loki"},
                  {"describe":"STL是Standard Template Library的简称，中文名标准模板库。STL即为通用算法+通用容器。","id":0,"link":"https://github.com/StupidTAO/STL","name":"STL"},
                  {"describe":"SimpleCion是一个简单的，不太安全，不太完整的用Python制作的区块链实现。这个项目的目标是制作一个有效的区块链货币，尽可能的使它简洁，可以用作教材。","id":0,"link":"https://github.com/StupidTAO/SimpleCoin","name":"SimpleCoin"},
                  {"describe":"这是一个基于第三方与服务开发的Android APP，本项目与Siri功能类似可与用户对话，查询天气、检索信息、智能聊天等功能。","id":0,"link":"https://github.com/StupidTAO/IceCream_Turling","name":"IceCream_Turling"},
-                 {"describe":"Python2包含了很多使用的小程序，Python2解决一些常见的问题，也为初学者提供学习。","id":0,"link":"https://github.com/StupidTAO/Python-2","name":"Python2"},
                  {"describe":"这个项目是用于聚合知名新闻平台上的NBA新闻，如hupu，腾讯，新浪等，为NBA球迷们提供全面、准确、权威的NBA新闻。","id":0,"link":"https://github.com/StupidTAO/nba-tall","name":"nba-tall"}],
  "wechat":{"hint":"扫一扫关注曹海涛","image":"./images/main/cht_wechat.png"}}
