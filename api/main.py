626868321ef9ce7847d44f1d8fc3acda142f7fa2
Haitao Cao <caohaitao_linux@buaa.edu.cn>
2020-07-09 11:24:17 +0800

    Update main.py
    add welfare_spider repo

--- api/main.py
+++ api/main.py
@@ -9,5 +9,5 @@
                  {"describe":"apigateway是一个轻量级的golang语言Api Gateway实现，公有链justitia对外提供的api接口，节点运行后链接至节点，即可获取节点服务","id":0,"link":"https://github.com/StupidTAO/apigateway","name":"Apigateway"},
                  {"describe":"SimpleCion是一个简单的，不太安全，不太完整的用Python制作的区块链实现。这个项目的目标是制作一个有效的区块链货币，尽可能的使它简洁，可以用作教材。","id":0,"link":"https://github.com/StupidTAO/SimpleCoin","name":"SimpleCoin"},
                  {"describe":"这是一个基于第三方与服务开发的Android APP，本项目与Siri功能类似可与用户对话，查询天气、检索信息、智能聊天等功能。","id":0,"link":"https://github.com/StupidTAO/IceCream_Turling","name":"IceCream_Turling"},
-                 {"describe":"这个项目是用于聚合知名新闻平台上的NBA新闻，如hupu，腾讯，新浪等，为NBA球迷们提供全面、准确、权威的NBA新闻。","id":0,"link":"https://github.com/StupidTAO/nba-tall","name":"nba-tall"}],
+                 {"describe":"python实现的爬虫代码，将爬取的流程模块化，便于修改和扩展。内置URL队列可将子URL放入队列，持续爬取，修改后可爬取任意网页数据。","id":0,"link":"https://github.com/StupidTAO/welfare_spider","name":"welfare_spider"}],
  "wechat":{"hint":"扫一扫关注曹海涛","image":"./images/main/cht_wechat.png"}}
